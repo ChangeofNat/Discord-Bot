39dee81ab234666b1b81562c3a16b6b644a77476
Soham <73393961+Pidgehut@users.noreply.github.com>
2021-04-06 22:00:14 +0530

    Create module.py

---
+++ module.py
@@ -0,0 +1,29 @@
+import math
+import random
+def add(a,b):
+  print (a+b)
+  return a+b
+def subtract (a,b):
+  print (a-b)
+  return a-b
+def multiply (a,b):
+  print (a*b)
+  return a*b
+def divide(a,b):
+  print(a/b)
+  return a/b
+def modulus(a,b):
+  print(a%b)
+  return a%b
+def squareroot(c):
+  print(math.sqrt(c))
+def square(d):
+  print(d*d)
+def average(e,f):
+  g=(e+f)/2 
+  print(g)
+def round_of(h):
+  print(math.ceil(h))
+def get_random_num(i):
+  print(random.randint(1,i))
+  
